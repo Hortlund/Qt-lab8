f212288590eb26f374ead4665982b074ca0f4e3e
Hortlund <hortlund.andreas@gmail.com>
2020-03-04 15:12:19 +0100

    ui done

--- deepdive/mainwindow.cpp
+++ deepdive/mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QInputDialog>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,3 +13,13 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    int count = ui->spinBox->value();
+    int comp [3];
+    for (int i = 0; i < count ; i++) {
+        QString str = QString::number(i + 1);
+        comp[i] = QInputDialog::getInt(this, "Component Ohm", "Komponent: " + str );
+    }
+}
