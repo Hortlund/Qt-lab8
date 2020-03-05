ccf02d792b10b0b7aca776cb6260a5c500093826
Hortlund <hortlund.andreas@gmail.com>
2020-03-05 14:50:54 +0100

    Task 3 done

--- deepdive/mainwindow.cpp
+++ deepdive/mainwindow.cpp
@@ -21,11 +21,35 @@ void MainWindow::on_pushButton_clicked()
 
     int count = ui->spinBox->value();
     float comp [3];
+    float arrayE [3] = {1.0, 1.0, 1.0};
+    int raknare = 0;
     for (int i = 0; i < count ; i++) {
         QString str = QString::number(i + 1);
         comp[i] = QInputDialog::getInt(this, "Component Ohm", "Komponent: " + str );
     }
-   QString res = QString::number(calc_resistance(3, 'S', comp));
-   ui->lineEdit_2->setText(res);
+
+    char koppling{};
+    if(ui->radioButton->isChecked()){
+        koppling = 'S';
+    }
+    else{
+        koppling = 'P';
+    }
+    float resistance = calc_resistance(count, koppling, comp);
+    QString res = QString::number(resistance);
+    ui->lineEdit_2->setText(res + " ohm");
+
+    float volt = ui->lineEdit->text().toFloat();
+    float power = calc_power_r(volt, resistance);
+    QString pow = QString::number(power);
+    ui->lineEdit_3->setText(pow + " W");
+
+    count = e_resistance(resistance, arrayE);
+    QString er;
+    while(raknare<count){
+        er += QString::number(arrayE[raknare]) + ", ";
+        raknare++;
+    }
+    ui->lineEdit_4->setText(er);
 
 }
